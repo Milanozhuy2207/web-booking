4a094f269b38f9956ebc3da5f9fc741664261024
Huy <huynguyenv0722@gmail.com>
2026-05-03 15:18:39 +0700

    fix: update vercel rewrites and vite manualChunks for better image rendering

--- vite.config.js
+++ vite.config.js
@@ -9,19 +9,13 @@ export default defineConfig({
     tailwindcss(),
   ],
   build: {
-    // 1. Nâng giới hạn lên 1000kb để không bị báo lỗi vặt với các thư viện lớn
     chunkSizeWarningLimit: 1000, 
-    
     rollupOptions: {
       output: {
-        // 2. Tiếp tục chia nhỏ thư viện để trình duyệt load song song cho nhanh
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // Tách các vendors lớn ra chunk riêng
             if (id.includes('react')) return 'vendor-react';
-            if (id.includes('lucide-react')) return 'vendor-icons';
-            
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            return 'vendor';
           }
         },
       },
